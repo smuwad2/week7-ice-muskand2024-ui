da7c5197c298ffad4d9a345b0cf4aab9acc06cb4
muskand2024-ui <muskan.d.2024@smu.edu.sg>
2025-10-21 00:54:36 +0000

    swenteoem

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,6 +2,27 @@
     export default { 
         // Add Code Here to complete the task
         // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+            data() {
+        return {
+            x: 0,
+            y: 0,
+            operators: ['+', '-', '*', '/'],
+            selectedOp: '+'
+        }
+    },
+    computed: {
+        result() {
+            const a = Number(this.x)
+            const b = Number(this.y)
+            switch (this.selectedOp) {
+                case '+': return a + b
+                case '-': return a - b
+                case '*': return a * b
+                case '/': return b === 0 ? '∞' : a / b
+                default: return NaN
+            }
+        }
+    }
     }
 </script>
 
